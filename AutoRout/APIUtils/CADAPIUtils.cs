035ae68b0364111db7f5d260f245c740aed99528
Huydev0808 <phamngohoanghuy95@gmail.com>
2024-01-04 16:51:05 +0700

    - Done feauture auto rout for AutoCAD

--- AutoRout/APIUtils/CADAPIUtils.cs
+++ AutoRout/APIUtils/CADAPIUtils.cs
@@ -8,38 +8,176 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
+using System.Windows.Forms;
+using Application = Autodesk.AutoCAD.ApplicationServices.Application;
 
 namespace AutoRout.APIUtils
 {
     public class CADAPIUtils
     {
-        public static void SelectBlock()
+        public static Tuple<double, double, double> SelectBlock(string blockName)
         {
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Editor editor = doc.Editor;
-
-            PromptEntityOptions promptOptions = new PromptEntityOptions("\nSelect a block reference: ");
+      
+            PromptEntityOptions promptOptions = new PromptEntityOptions($"\nSelect a {blockName}: ");
             promptOptions.SetRejectMessage("\nInvalid selection. Please select a block reference.");
             promptOptions.AddAllowedClass(typeof(BlockReference), true);
-            
+      
             PromptEntityResult promptResult = editor.GetEntity(promptOptions);
 
             if (promptResult.Status == PromptStatus.OK)
             {
+                
+                ObjectId selectedObjectID = promptResult.ObjectId;
                 using (Transaction transaction = doc.TransactionManager.StartTransaction())
                 {
                     BlockReference blockReference = transaction.GetObject(promptResult.ObjectId, OpenMode.ForRead) as BlockReference;
 
                     if (blockReference != null)
                     {
-                        // Perform operations with the selected block reference
+                        //editor.SetImpliedSelection(new ObjectId[] { });
+                        editor.SetImpliedSelection(new ObjectId[] { selectedObjectID });
                         editor.WriteMessage($"\nSelected block reference: {blockReference.Name}");
-                    }
-
+                        double xPosition = blockReference.Position.X;
+                        double yPosition = blockReference.Position.Y;
+                        double rotation = blockReference.Rotation;
+                        return new Tuple<double, double, double>(xPosition, yPosition, rotation);
+                    }                   
                     transaction.Commit();
                 }
             }
+            return null;
+        }
+        public static void DrawRout3Point (double pointXFaSu, double pointYFaSu, 
+            double pointXConnector, double pointYConnector, double pointXIntersect, double pointYIntersect)
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
 
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Block table for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                                OpenMode.ForRead) as BlockTable;
+
+                // Open the Block table record Model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                                                OpenMode.ForWrite) as BlockTableRecord;
+
+                // Create a polyline with two segments (3 points)
+                using (Polyline acPoly = new Polyline())
+                {
+                    acPoly.AddVertexAt(0, new Point2d(pointXFaSu, pointYFaSu), 0, 0, 0);
+                    acPoly.AddVertexAt(1, new Point2d(pointXIntersect, pointYIntersect), 0, 0, 0);
+                    acPoly.AddVertexAt(2, new Point2d(pointXConnector, pointYConnector), 0, 0, 0);
+
+                    // Add the new object to the block table record and the transaction
+                    acBlkTblRec.AppendEntity(acPoly);
+                    acTrans.AddNewlyCreatedDBObject(acPoly, true);
+                }
+
+                // Save the new object to the database
+                acTrans.Commit();
+            }
+        }
+        public static void DrawRout4Point (double pointXFaSu, double pointYFaSu,
+            double pointXConnector, double pointYConnector, double xExtend, double yExtend, double pointXIntersect, double pointYIntersect)
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Block table for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                                OpenMode.ForRead) as BlockTable;
+
+                // Open the Block table record Model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                                                OpenMode.ForWrite) as BlockTableRecord;
+
+                // Create a polyline with two segments (3 points)
+                using (Polyline acPoly = new Polyline())
+                {
+                    acPoly.AddVertexAt(0, new Point2d(pointXFaSu, pointYFaSu), 0, 0, 0);
+                    acPoly.AddVertexAt(1, new Point2d(xExtend, yExtend), 0, 0, 0);
+                    acPoly.AddVertexAt(2, new Point2d(pointXIntersect, pointYIntersect), 0, 0, 0);
+                    acPoly.AddVertexAt(3, new Point2d(pointXConnector, pointYConnector), 0, 0, 0);
+
+                    // Add the new object to the block table record and the transaction
+                    acBlkTblRec.AppendEntity(acPoly);
+                    acTrans.AddNewlyCreatedDBObject(acPoly, true);
+                }
+
+                // Save the new object to the database
+                acTrans.Commit();
+            }
+        }
+        public static void DrawRout5Point (double pointXFaSu, double pointYFaSu,
+            double pointXConnector, double pointYConnector, double xFaSuExtend, double yFaSuExtend, double xConnectorExt, double yConnectorExt, double pointXIntersect, double pointYIntersect)
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Block table for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
+                                                OpenMode.ForRead) as BlockTable;
+
+                // Open the Block table record Model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
+                                                OpenMode.ForWrite) as BlockTableRecord;
+
+                // Create a polyline with two segments (3 points)
+                using (Polyline acPoly = new Polyline())
+                {
+                    acPoly.AddVertexAt(0, new Point2d(pointXFaSu, pointYFaSu), 0, 0, 0);
+                    acPoly.AddVertexAt(1, new Point2d(xFaSuExtend, yFaSuExtend), 0, 0, 0);
+                    acPoly.AddVertexAt(2, new Point2d(pointXIntersect, pointYIntersect), 0, 0, 0);
+                    acPoly.AddVertexAt(3, new Point2d(xConnectorExt, yConnectorExt), 0, 0, 0);
+                    acPoly.AddVertexAt(4, new Point2d(pointXConnector, pointYConnector), 0, 0, 0);
+
+                    // Add the new object to the block table record and the transaction
+                    acBlkTblRec.AppendEntity(acPoly);
+                    acTrans.AddNewlyCreatedDBObject(acPoly, true);
+                }
+
+                // Save the new object to the database
+                acTrans.Commit();
+            }
+        }
+        public static double PlusXDriect (double xCoordinate)
+        {
+            return xCoordinate + 24;
+        }
+        public static double PlusYDirect(double yCoordinate)
+        {
+            if(yCoordinate > 0)
+            {
+                return yCoordinate + 24;
+            }
+            else if(yCoordinate < 0)
+            {
+                return yCoordinate - 24;
+            }
+            return yCoordinate;
         }
     }
 }
--- AutoRout/AutoRout.cs
+++ AutoRout/AutoRout.cs
@@ -16,7 +16,95 @@ namespace AutoRout
         [CommandMethod("ArentAutoRout")]
         public static void Autorout()
         {
-            CADAPIUtils.SelectBlock();
+            double ExtensValue = 24;
+            //Select Fasu
+            Tuple<double, double, double> getFasu = CADAPIUtils.SelectBlock("Fasu");
+            if (getFasu != null)
+            {
+                double xFasuCoord = getFasu.Item1;
+                double yFasuCoord = getFasu.Item2;
+                double rotationFasu = getFasu.Item3 * (180 / Math.PI);
+
+                //Select Connector
+                Tuple<double, double, double> getConnector = CADAPIUtils.SelectBlock("Connector");
+                if (getConnector != null)
+                {
+                    double xConnectorCoord = getConnector.Item1;
+                    double yConnectorCoord = getConnector.Item2;
+                    double rotationConnector = getConnector.Item3 * (180 / Math.PI);
+
+                    if (rotationFasu == 0 && rotationConnector == 0)
+                    {
+                        //Find Intersecting Point of Fasu and Connector C(xC, yC)
+                        double xC = xConnectorCoord;
+                        double yC = yFasuCoord;
+                        CADAPIUtils.DrawRout3Point(xFasuCoord, yFasuCoord, xConnectorCoord, yConnectorCoord, xC, yC);
+                    }
+                    else if (rotationFasu == 0 && rotationConnector == 90.0)
+                    {
+                        //Defind extension point xFasuCoord + ExtDistance
+                        double xExt = CADAPIUtils.PlusXDriect(xFasuCoord);
+                        double yExt = yFasuCoord;
+                        //Find Intersecting Point of Fasu and Connector C(xC, yC)
+                        double xC = xExt;
+                        double yC = yConnectorCoord;
+                        CADAPIUtils.DrawRout4Point(xFasuCoord, yFasuCoord, xConnectorCoord, yConnectorCoord, xExt, yExt, xC, yC);
+                    }
+                    else if (rotationFasu == 0 && rotationConnector == 180.0)
+                    {
+                        //Defind extension point xFasuCoord + ExtDistance
+                        double xExtFasu = CADAPIUtils.PlusXDriect(xFasuCoord);
+                        double yExtFasu = yFasuCoord;
+                        //Defind extension point yConnectorCoord + ExtDistance
+                        double xExtConnector = xConnectorCoord;
+                        double yExtConnector = yConnectorCoord -24;
+                        //Find Intersecting Point of Fasu and Connector C(xC, yC)
+                        double xC = xExtFasu;
+                        double yC = yExtConnector;
+
+                        CADAPIUtils.DrawRout5Point(xFasuCoord, yFasuCoord, xConnectorCoord, yConnectorCoord, xExtFasu, yExtFasu, xExtConnector, yExtConnector, xC, yC);
+                    }
+                    else if (rotationFasu == 90 && rotationConnector == 180)
+                    {
+                        //Defind extension point yFasuCoord + ExtDistance
+                        double xExtFasu = xFasuCoord;
+                        double yExtFasu = yFasuCoord + ExtensValue;
+                        //Find Intersecting Point of Fasu and Connector C(xC, yC)
+                        double xC = xConnectorCoord;
+                        double yC = yExtFasu;
+
+                        CADAPIUtils.DrawRout4Point(xFasuCoord, yFasuCoord, xConnectorCoord, yConnectorCoord, xExtFasu, yExtFasu, xC, yC);
+                    }
+                    else if (rotationFasu == 270 && rotationConnector == 270)
+                    {
+                        double xC = xFasuCoord;
+                        double yC = yConnectorCoord;
+                        CADAPIUtils.DrawRout3Point(xFasuCoord, yFasuCoord, xConnectorCoord, yConnectorCoord, xC, yC);
+                    }
+                    else if (rotationFasu == 270 && rotationConnector == 0)
+                    {
+                        double xExtFasu = xFasuCoord;
+                        double yExtFasu = yFasuCoord - 24;
+                        double xC = xConnectorCoord;
+                        double yC = yExtFasu;
+
+                        CADAPIUtils.DrawRout4Point(xFasuCoord, yFasuCoord, xConnectorCoord, yConnectorCoord, xExtFasu, yExtFasu, xC, yC);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+
+                //CADAPIUtils.SelectBlock("Fasu");
+                //Select Connector
+                //CADAPIUtils.SelectBlock("Connector");
+
+            }
         }
     }
 }
